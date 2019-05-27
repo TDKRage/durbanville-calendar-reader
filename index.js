ca20275395fd503af14034d9bba1b327d37fbdc0
Lyle Classen <lylec@globalkinetic.com>
2019-05-27 20:53:38 +0200

    preogressing

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 // https://sheetjs.gitbooks.io/docs
-const xlsx = require('xlsx');
+const { readFile, utils } = require('xlsx');
 
-const parseGeneralProgramme = (programme) => {
-  const { SheetNames, Sheets} = xlsx.readFile(programme); 
-  
-  const currentMonthSheetName = SheetNames[SheetNames.length -2];
-  
-  console.log(currentMonthSheetName);
 
-  return currentMonthSheetName;
+const findWeekRowStart = (sheet) => {
+  for (let index = 0; index < 20; index++) {
+    let cell = { c: 0 , r: index };
+
+    const encodedCell = utils.encode_cell(cell);
+
+    if(sheet[encodedCell] && sheet[encodedCell].v === 'Sunday'){
+      return cell;
+    }
+  }
+  return undefined;
+}
+
+const isEndingCell = (cell) => {
+  console.log(cell);
+   if(cell) {
+    return !isNaN(cell.w);
+   }
+   return false; 
+}
+
+const getDayEndRow = (sheet, startRow) => {
+  for (let index = startRow.r; index < startRow.r + 10; index++) {
+    const cell = { ...startRow, r: index };
+    if(isEndingCell(sheet[utils.encode_cell(cell)])){
+      
+      return { ...cell, r: cell.r -1 };
+    } 
+  }
+}
+
+const splitIntoDays = (sheet, weekRowStart) => {
+  const startRowCell = { ...weekRowStart, r: weekRowStart.r + 1 };
+  const dayEndRowCell = getDayEndRow(sheet, startRowCell);
+  console.log('dayEndRowCell', dayEndRowCell);
+}
+
+const parseGeneralProgramme = (sheet) => {
+  const weekRowStart = findWeekRowStart(sheet); 
+
+  const daySplitArray = splitIntoDays(sheet, weekRowStart);
+
+  return [];
 }; 
 
-module.exports = { parseGeneralProgramme };
+module.exports = { 
+  parseGeneralProgramme,
+  findWeekRowStart,
+  getDayEndRow,
+};
--- index.test.js
+++ index.test.js
@@ -1,18 +1,44 @@
 const path = require('path');
-const {parseGeneralProgramme} = require('.');
+const { readFile, utils } = require('xlsx');
+const { 
+  parseGeneralProgramme,
+  findWeekRowStart,
+  getDayEndRow,
+} = require('.');
 
 describe('Durbanville Calendar Manager', () => {
-  describe('parseGeneralProgramme', () => {
+  describe('May 2019 Programme', () => {
     const testFilePath = path.resolve(__dirname, 'test-data','general-programme-may-2019.xlsx');
-    let output; 
-    beforeAll(() => {
-      output = parseGeneralProgramme(testFilePath);
+    const { SheetNames, Sheets} = readFile(testFilePath);
+    const currentMonthSheetName = SheetNames[SheetNames.length-2];
+    const sheet = Sheets[currentMonthSheetName]; 
+   
+    describe('parseGeneralProgramme', () => {
+      let output; 
+      beforeAll(() => {
+        output = parseGeneralProgramme(sheet);
+      });
+      test('should be defined', () => {
+        expect(output).toBeDefined();
+      });
+     test.skip('should have more than 28 entries', () => {
+        expect(output.length).toBeGreaterThan(28);
+      });
     });
-    test('should be defined', () => {
-      expect(output).toBeDefined();
+    describe('findWeekRowStart', () => {
+      test('should should be A3', () => {
+       const result = findWeekRowStart(sheet);
+       expect(utils.encode_cell(result)).toEqual('A2');
+      });
     });
-    test('should have length greater than 0', () => {
-      expect(output.length).toBeGreaterThan(0);
+    describe('getDayEndRow', () => {
+      const weekRowStart = findWeekRowStart(sheet); 
+      const startRowCell = { ...weekRowStart, r: weekRowStart.r + 1 };
+
+      const result = getDayEndRow(sheet, startRowCell);
+      test('should should be A8', () => {
+        expect(utils.encode_cell(result)).toEqual('A8');
+      });
     });
   });
 });
